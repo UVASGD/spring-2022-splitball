51ea3162af96460cf8b0f61f9df1a9a7b173b0ab
Chris R <CMRCMR12345@gmail.com>
2022-02-27 17:46:42 -0500

    added dashes for both players and bug fixes
    -added dashes to each player using q and u with charges for each under player data
    -fixed bug on how player 2 was recording losses which allowed out of bounds
    -fixed player 2 audio bug
    -organized and created prefabs for player 1 and 2 and powerup manager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -26,6 +26,7 @@ public class PlayerController : Destructible
 
     //im sorta copying this from last year's project im 90% sure some of it is not necessary
     Rigidbody2D rb2d;
+    PlayerData stats;
     public float currentMaxSpeed;
     public float maxMoveSpeed = 10f;
     public float maxDashSpeed = 20f;
@@ -33,8 +34,6 @@ public class PlayerController : Destructible
     public float dashCD = 1f;
     public float lastDash = 0f;
     public float dashPower = 10f;
-    public GameObject crosshairs;
-    public float crosshairDistance = 4;
     public Sprite neutral;
     public Sprite dash;
     public Transform pos;
@@ -59,6 +58,7 @@ public class PlayerController : Destructible
     // Start is called before the first frame update
     void Start()
     {
+        stats = GetComponent<PlayerData>();
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         RecallActive = false;
@@ -68,10 +68,12 @@ public class PlayerController : Destructible
     void Update()
     {
         if (gm.isActive) {
+                        
             movement = new Vector2(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1"));
-            aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-         //   fire = Input.GetMouseButton(0);  
-         //   InvokeRepeating("RegenerateStamina", 0f, .5f);
+            fire = Input.GetKey("q");
+            aim = rb2d.velocity.normalized;
+            //   InvokeRepeating("RegenerateStamina", 0f, .5f);
+
         }
         iFrameCounter += Time.deltaTime;
         lastDash += Time.deltaTime;
@@ -83,36 +85,28 @@ public class PlayerController : Destructible
         //NOTE FOR FUTURE DEVELOPING:
         //to make a "dash" use rb2d.AddForce(new Vector2(speed,speed), ForceMode2D.Impulse)
 
-        if(fire && lastDash >= dashCD){
+        if(fire && lastDash >= dashCD && stats.dashes != 0){
             StartCoroutine(Deccelerate());
             AudioSource.PlayClipAtPoint(clips[0], transform.position);
             rb2d.velocity = new Vector2(0,0);
-            rb2d.AddForce(aim.normalized * crosshairDistance * dashPower, ForceMode2D.Impulse);
+            rb2d.AddForce(aim.normalized * dashPower, ForceMode2D.Impulse);
             lastDash = 0f;
+            stats.dashes -= 1;
         }
 
 
         rb2d.AddForce(movement * movePower);
-        //rb2d.velocity += (.00000001 + movement) * movePower;
-        //if(Vector2.magnitude(rb2d.velocity) > maxSpee)
+      
         if(rb2d.velocity.magnitude > currentMaxSpeed){
             //note: using velocity makes it easily push physics objects away instead of bouncing off of them (as intended)2
                 rb2d.velocity = rb2d.velocity.normalized * currentMaxSpeed;
         }
 
-		Debug.DrawRay(transform.position, aim.normalized * crosshairDistance, Color.red);
-
         Vector2 dir = rb2d.velocity;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         animator.SetFloat("Angle", angle);
         animator.SetFloat("VelMag", rb2d.velocity.magnitude);
-		// position crosshairs
-		//if(aim.magnitude < crosshairDistance) { 
-			//crosshairs.transform.position = (Vector2) transform.position + aim;
-		//} else {
-		crosshairs.transform.position = (Vector2) transform.position + (aim.normalized * crosshairDistance);
-		//}
-	}
+    	}
 
     private void OnTriggerEnter2D(Collider2D collision) {
 
